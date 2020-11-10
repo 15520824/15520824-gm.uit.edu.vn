0d705a1536c86b9a492ebcc4dfc1616dbb79359c
15520824@gm.uit.edu.vn <hokbit1S>
2020-11-10 12:08:31 +0700

    update Table tree

--- js/component/ModuleView.js
+++ js/component/ModuleView.js
@@ -2416,6 +2416,7 @@ tableView.prototype.getRow = function(data) {
 
 tableView.prototype.setDisPlay = function() {
     if (!this.classList.contains("more-child") && this.data.child && this.data.child.length !== this.childrenNodes.length) {
+        this.data.updateVisible = undefined;
         for (var i = 0; i < this.childrenNodes.length; i++) {
             this.childrenNodes[i].selfRemove();
         }
